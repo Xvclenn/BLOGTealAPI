0ec9b89fcd9c527f8bbcc4b9b76290c84c3d9197
Xvclenn <benhhslen@gmail.com>
2023-02-07 15:06:21 +0800

    ...

---
+++ main.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+const port = 247;
+const app = express();
+
+let blogList = [];
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/blogList', (req, res) => {
+    res.json(blogList);
+});
+
+app.get('/blogList/:id', (req, res) => {
+    const { id } = req.params;
+    const one = blogList.find((blog) => blog.id === id);
+    if (one) {
+        res.json(one);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/blogList', (req, res) => {
+    const { title, author, img, blogBody, date } = req.body;
+
+    const newBlog = {
+        id: uuidv4(),
+        title,
+        author,
+        blogBody,
+        img,
+        date,
+    };
+    blogList.push(newBlog);
+    res.json(blogList);
+});
+
+app.listen(port, () => {
+    console.log('OK', port);
+});
