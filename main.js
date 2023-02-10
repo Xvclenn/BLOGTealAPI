f3cbaac71f13ccf442e18824375ce7f226b2fdc7
Xvclenn <https://github.com/Xvclenn>
2023-02-10 17:45:38 +0800

    ...

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 const port = 2470;
 const app = express();
-
-let blogList = [];
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/blogList", (req, res) => {
+    const blogList = readBlogList();
     res.json(blogList);
 });
 
+function readBlogList() {
+    const blog = fs.readFileSync("blogList.json");
+    const blogList = JSON.parse(blog);
+    return blogList;
+}
+
 app.get("/blogList/:id", (req, res) => {
     const { id } = req.params;
+    const blogList = readBlogList();
     const one = blogList.find((blog) => blog.id === id);
     if (one) {
         res.json(one);
@@ -35,12 +42,19 @@ app.post("/blogList", (req, res) => {
         img,
         date,
     };
+
+    const blogList = readBlogList();
+
     blogList.unshift(newBlog);
 
+    fs.writeFileSync("blogList.json", JSON.stringify(blogList));
+
     res.json(blogList);
+    res.sendStatus(201);
 });
 
 app.delete("/blogList/:id", (req, res) => {
+    const blogList = readBlogList();
     const { id } = req.params;
     const one = blogList.find((blog) => blog.id === id);
     if (one) {
